fb34b6ddf3889e46d083c0a4b01dc1fcdac23be7
ketchupanezz <cheezsauce@mail.ru>
2025-03-19 19:09:26 +0300

    init: start task

---
+++ src/components/promo/Promo.jsx
@@ -0,0 +1,13 @@
+const Promo = () => {
+    return ( <section className="promo">
+        <div className="container">
+            <div className="promo__content">
+                <div className="promo__text">TEXT</div>
+                <div className="promo__img">Img</div>
+            </div>
+        </div>
+
+    </section> );
+}
+ 
+export default Promo;
